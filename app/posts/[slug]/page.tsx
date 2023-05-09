2e8d5a47e4aff633faed161a2223f97bc5c1cc2a
Jannik <jannik.schroeder05@gmail.com>
2023-05-09 17:23:15 +0200

    Added the layout to all child page.tsx

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -23,25 +23,25 @@ const PostPage = (props: any) => {
     const slug = props.params.slug;
     const post = getPostContent(slug);
     return (
-        <div className="bg-primary justify-center">
-            <div className="container mx-auto px-4 py-8 md:py-16 h-full flex items-center justify-center">
-                <div className="flex flex-col md:flex-row items-center justify-center md:justify-between w-full">
-                    <div className="text-primary text-center md:text-left mb-8 md:mb-0">
-                        <div>
-                            <div className="my-12 text-center">
-                                <h1 className="text-2xl text-slate-600 text-primary">{post.data.title}</h1>
-                                <p className="text-slate-400 mt-2 text-primary">{post.data.date}</p>
+        <div className="container lg:max-w-screen-lg mx-auto px-6 break-all">
+            <div className="bg-primary justify-center">
+                <div className="container mx-auto px-4 py-8 md:py-16 h-full flex items-center justify-center">
+                    <div className="flex flex-col md:flex-row items-center justify-center md:justify-between w-full">
+                        <div className="text-primary text-center md:text-left mb-8 md:mb-0">
+                            <div>
+                                <div className="my-12 text-center">
+                                    <h1 className="text-2xl text-slate-600 text-primary">{post.data.title}</h1>
+                                    <p className="text-slate-400 mt-2 text-primary">{post.data.date}</p>
+                                </div>
+                                <article className="prose text-primary">
+                                    <Markdown>{post.content}</Markdown>
+                                </article>
                             </div>
-
-                            <article className="prose text-primary">
-                                <Markdown>{post.content}</Markdown>
-                            </article>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-
     );
 };
 
