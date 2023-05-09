c69734fe137aefc088bec8c9796884b39c79e0bc
schr0der <jannik.schroeder05@gmail.com>
2023-05-09 09:58:20 +0200

    Added Projects site and Markdown Support for the posts.

---
+++ app/projects/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+    return (
+        <div className="bg-primary text-primary min-h-screen">
+            <section>
+                <h1>Page</h1>
+            </section>
+        </div>
+    )
+}
